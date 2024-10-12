c41b7273fbdc2683517b36a8eb2883042bc83fa2
Lmakinaa <jaija.ismail@gmail.com>
2024-10-12 12:37:05 +0100

    Refactor: CGI implementation

--- srcs/CGI/CGI.cpp
+++ srcs/CGI/CGI.cpp
@@ -3,7 +3,7 @@
 static void readOutput(int fd, std::string& buff)
 {
     char* cbuff; 
-    try {cbuff = new char[BUFF_SIZE]();} catch(std::exception&e) {return ;}
+    try {cbuff = new char[BUFF_SIZE]();} catch(std::exception&e) {return ;} // No response if fail
     size_t count = 0;
     while (1) {
         int r = read(fd, cbuff, BUFF_SIZE);
@@ -11,12 +11,12 @@ static void readOutput(int fd, std::string& buff)
         if (r <= 0) {
             if (r == -1 && M_DEBUG)
                 perror("execve(2)");
-            break ;
+            break ; // No response
         }
 
         count += r;
         if (count > buff.max_size())
-            break ;
+            break ; // No response
 
         buff.append(cbuff, r);
         std::memset(cbuff, 0, r + 1);
@@ -26,10 +26,20 @@ static void readOutput(int fd, std::string& buff)
     delete[] cbuff;
 }
 
+static void closePipe(int fds[2])
+{
+    close(fds[0]);
+    close(fds[1]);
+}
+
 void CGI::scriptToHtml(const char* cgiPath, const char* argv[], std::string& buff)
 {
     int fds[2];
-    pipe(fds);
+    if (pipe(fds) == -1) {
+        if (M_DEBUG)
+            perror("pipe(2)");
+        return ; // No reponse
+    }
 
     int pid = fork();
     if (!pid)
@@ -41,44 +51,45 @@ void CGI::scriptToHtml(const char* cgiPath, const char* argv[], std::string& buf
             exit(1);
         }
     }
-    else if (pid == -1)
-    {
-        if (M_DEBUG)
-            perror("fork(2)");
-    }
     else
     {
-        close(fds[1]);
+        if (pid == -1) {
+            if (M_DEBUG)
+                perror("fork(2)");
+            return (closePipe(fds)); // No response
+        }
         if (waitpid(pid, NULL, 0) == -1)
         {
             if (M_DEBUG)
                 perror("waitpid(2)");
+            return (closePipe(fds)); // No response
         }
         else
         {
+            close(fds[1]);
             readOutput(fds[0], buff);
+            close(fds[0]);
         }
     }
-
 }
 
-int main()
-{
-    const char* cgiPath = "/usr/bin/php";
-    const char* cgi = "php";
-    const char* file = "/Users/ijaija/web-server/srcs/CGI/test.php";
-    const char* argv[3];
-    argv[0] = cgi;
+// int main()
+// {
+//     const char* cgiPath = "/usr/bin/php";
+//     const char* cgi = "php";
+//     const char* file = "/Users/ijaija/web-server/srcs/CGI/test.php";
+//     const char* argv[3];
+//     argv[0] = cgi;
     
 
-    argv[1] = file;
-    argv[2] = NULL;
+//     argv[1] = file;
+//     argv[2] = NULL;
 
-    std::string buff;
+//     std::string buff;
 
-    CGI::scriptToHtml(cgiPath, argv, buff);
+//     CGI::scriptToHtml(cgiPath, argv, buff);
 
-    std::cout << buff << std::endl;
-    return 0;
+//     std::cout << buff << std::endl;
+//     return 0;
 
-}
+// }
